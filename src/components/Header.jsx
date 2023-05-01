f46d6756da648652f516cc18cbd233221373147c
Mahadi90 <mahadimhs787@gmail.com>
2023-05-02 00:22:58 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <Link to='/'>Home</Link>
+        <Link to='/blog'>Blog</Link>
+        <Link>Contact</Link>
+      </ul>
+    </div>
+    <a className="btn btn-ghost normal-case text-xl">Terrific Chef</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+    <Link className='mx-2' to='/'>Home</Link>
+        <Link className='mx-2'  to='/blog'>Blog</Link>
+        <Link className='mx-2' >Contact</Link>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <Link className='me-8 bg-red-400 py-1 px-3 rounded-md text-white'  to='/login'>Login</Link>
+  </div>
+</div>
     );
 };
 
